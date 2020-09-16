f0b7a31cedcd542eda25b28ec24b49e037e01ee3
Xiaoyangy <949666125@qq.com>
2020-09-16 16:40:34 +0800

    fir

---
+++ feign/src/main/java/com/chy/feign/client/DataClient.java
@@ -0,0 +1,14 @@
+package com.chy.feign.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(value = "TEST-PROVIDER")
+@RequestMapping("provider")
+public interface DataClient {
+
+    @RequestMapping("get")
+    public String getData();
+}
+ 
---
+++ server_provide/src/test/java/com/chy/server_provide/ServerProvideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chy.server_provide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProvideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
